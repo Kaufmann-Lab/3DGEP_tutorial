5f725ebe9aa59f8f56f3479a9e954f80539f0333
rywet56 <manuel.neumann.lauf@gmail.com>
2023-05-28 09:59:11 +0000

    corrected paths

--- env_setup/build_docker_image.sh
+++ env_setup/build_docker_image.sh
@@ -1,5 +1,5 @@
 echo $working_dir
 
-cd "${working_dir}/3D_gene_expression_prediction/docker_files"
-sudo docker build ${working_dir}/3D_gene_expression_prediction/docker_files -f \
+cd "${working_dir}/3DGEP_tutorial/docker_files"
+sudo docker build ${working_dir}/3DGEP_tutorial/docker_files -f \
 jupyter_lab_novosparc_dockerfile -t jupyter_lab_novosparc
